443d8bd2d55ac2d6d61a10e16e6d1906595fa2db
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2025-03-18 15:49:32 +0530

    Minute fix

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,4 @@ app.listen(5000, () => {
   console.log('Server is running on port 5000. Access it using http://localhost:5000');
 });
 
-module.exports = (req, res) => {
-  app(req, res);
-};
+module.exports = app;
